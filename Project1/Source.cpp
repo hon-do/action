11a17a0ece03e9d4fad27028182b984a447e3324
hon-do <53286809+hon-do@users.noreply.github.com>
2020-01-31 05:01:29 +0900

    animation

--- Project1/Source.cpp
+++ Project1/Source.cpp
@@ -2,17 +2,30 @@
 
 void display(void) {
 	glutWireTeapot(1);// GLdouble size
-	glFlush();
+	glutSwapBuffers();// doublebufferÇÃêÿÇËë÷Ç¶
 }
 
+void (idle)(void) {
+	//êFclear
+	glClear(GL_COLOR_BUFFER_BIT);//GLbitfield mask
+	//âÒì]
+	glRotatef(
+		1,// GLfloat angle
+		0, 0, 1);// GLfloat x, GLfloat y, GLfloat z
+
+	glutPostRedisplay();
+}
 
 int main(int argc, char* argv[]) {
 	glutInit(
 		&argc,// int *argcp
 		argv);// char **argv
-	glutInitWindowPosition(640,0);// int x, y
+	//doublebufferingÅ´
+	glutInitDisplayMode(GL_DOUBLE);// unsigned int mode
+	glutInitWindowPosition(640, 0);// int x, y
 	glutInitWindowSize(640, 640);// int width, height
 	glutCreateWindow("title");// const char *title
 	glutDisplayFunc(display);// void (GLUTCALLBACK *func)(void)
+	glutIdleFunc(idle);// void (GLUTCALLBACK *func)(void)
 	glutMainLoop();
 }
