8b25cb52ab76dbe5ac5b4f562ab3926180c5df27
hon-do <53286809+hon-do@users.noreply.github.com>
2020-02-01 06:44:59 +0900

    timer関数

--- Project1/Source.cpp
+++ Project1/Source.cpp
@@ -1,11 +1,6 @@
 #include "glut.h"
 
 void display(void) {
-	glutWireTeapot(1);// GLdouble size
-	glutSwapBuffers();// doublebufferÇÃêÿÇËë÷Ç¶
-}
-
-void (idle)(void) {
 	//êFclear
 	glClear(GL_COLOR_BUFFER_BIT);//GLbitfield mask
 	//âÒì]
@@ -13,8 +8,30 @@ void (idle)(void) {
 		1,// GLfloat angle
 		0, 0, 1);// GLfloat x, GLfloat y, GLfloat z
 
-	glutPostRedisplay();
+	glutWireTeapot(1);// GLdouble size
+	glutSwapBuffers();// doublebufferÇÃêÿÇËë÷Ç¶
 }
+
+void (timer)(int value) {
+	glutPostRedisplay();
+
+	glutTimerFunc(
+		1000 / 60,// unsigned int millis
+		timer,	// void (GLUTCALLBACK *func)(int value)
+		0);		// int value
+}
+
+
+//void (idle)(void) {
+//	//êFclear
+//	glClear(GL_COLOR_BUFFER_BIT);//GLbitfield mask
+//	//âÒì]
+//	glRotatef(
+//		1,// GLfloat angle
+//		0, 0, 1);// GLfloat x, GLfloat y, GLfloat z
+//
+//	glutPostRedisplay();
+//}
 
 int main(int argc, char* argv[]) {
 	glutInit(
@@ -26,6 +43,11 @@ int main(int argc, char* argv[]) {
 	glutInitWindowSize(640, 640);// int width, height
 	glutCreateWindow("title");// const char *title
 	glutDisplayFunc(display);// void (GLUTCALLBACK *func)(void)
-	glutIdleFunc(idle);// void (GLUTCALLBACK *func)(void)
+	glutTimerFunc(
+		0,		// unsigned int millis
+		timer,	// void (GLUTCALLBACK *func)(int value)
+		0);		// int value
+
+	//glutIdleFunc(idle);// void (GLUTCALLBACK *func)(void)
 	glutMainLoop();
 }
